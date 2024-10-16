bc6595c5c82f336f62ce1690ce8945653a2e4796
ashkan73 <ashkansabbaghi73@gmail.com>
2024-10-16 18:51:38 +0330

    feat (project ) create projects and config tailwind  and create page and layout

---
+++ pages/blogs.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'blog',
+  title: 'blogs nipoto',
+  description: 'blogs for nipoto',
+})
+
+</script>
+
+<template>
+  <div class="text-3xl text-green-500 font-bold underline">page blogs </div>
+</template>
+
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="font-bold text-green-500">page 1 </div>
+</template>
+
